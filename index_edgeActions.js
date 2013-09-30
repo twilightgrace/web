d54a2e4d2b7c50f5c780ee6670a0fca54ffd407e
twilightgrace <smiley_life513@hotmail.com>
2013-09-30 18:16:27 +0800

    test_1

---
+++ index_edgeActions.js
@@ -0,0 +1,16 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play("loop");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Pii_Chan_Body'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play("loop");});
+//Edge binding end
+})("Pii_Chan_Body");
+//Edge symbol end:'Pii_Chan_Body'
+})(jQuery,AdobeEdge,"EDGE-10444433");
